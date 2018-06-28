6df49f13ee78d34b0e1b35c708eba9dd3243f8c1
Russ Miles <russ@russmiles.com>
2018-06-28 22:15:25 +0100

    Added overwriting of the settings file with new settings
    Signed-off-by: Russ Miles <russ@russmiles.com>

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os.path
-from chaoslib.settings import load_settings
+from chaoslib.settings import load_settings, save_settings
 
 settings_dir = os.path.join(os.path.dirname(__file__), "fixtures")
 
@@ -13,3 +13,16 @@ def test_do_not_fail_when_settings_do_not_exist():
 def test_load_settings():
     settings = load_settings(os.path.join(settings_dir, "settings.yaml"))
     assert "notifications" in settings
+
+
+def test_save_settings():
+    settings = load_settings(os.path.join(settings_dir, "settings.yaml"))
+    new_settings_location =  os.path.join(settings_dir, "new_settings.yaml")
+    try:
+        os.remove(new_settings_location)
+    except OSError:
+        pass
+    save_settings(settings, new_settings_location)
+    saved_settings = load_settings(new_settings_location)
+    assert "notifications" in saved_settings
+    os.remove(new_settings_location)
