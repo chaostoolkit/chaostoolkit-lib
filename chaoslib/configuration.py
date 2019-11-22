426aaac7d4cf22319536b9f53b960e2ee7d4f115
David Martin <dmartin35@users.noreply.github.com>
2019-11-22 09:18:36 +0100

    Optional default value for environment variable in configuration (#138)
    Signed-off-by: David Martin <david@chaoiq.io>

--- chaoslib/configuration.py
+++ chaoslib/configuration.py
@@ -16,6 +16,10 @@ def load_configuration(config_info: Dict[str, str]) -> Configuration:
     key strings to value as strings or dictionaries. In the former case, the
     value is used as-is. In the latter case, if the dictionary has a key named
     `type` alongside a key named `key`.
+    An optional default value is accepted for dictionary value with a key named
+    `default`. The default value will be used only if the environment variable
+    is not defined.
+
 
     Here is a sample of what it looks like:
 
@@ -25,13 +29,20 @@ def load_configuration(config_info: Dict[str, str]) -> Configuration:
         "token": {
             "type": "env",
             "key": "MY_TOKEN"
+        },
+        "host": {
+            "type": "env",
+            "key": "HOSTNAME",
+            "default": "localhost"
         }
     }
     ```
 
     The `cert` configuration key is set to its string value whereas the `token`
     configuration key is dynamically fetched from the `MY_TOKEN` environment
-    variable.
+    variable. The `host` configuration key is dynamically fetched from the
+    `HOSTNAME` environment variable, but if not defined, the default value
+    `localhost` will be used instead.
     """
     logger.debug("Loading configuration...")
     env = os.environ
@@ -41,11 +52,12 @@ def load_configuration(config_info: Dict[str, str]) -> Configuration:
         if isinstance(value, dict) and "type" in value:
             if value["type"] == "env":
                 env_key = value["key"]
-                if env_key not in env:
+                env_default = value.get("default")
+                if env_key not in env and not env_default:
                     raise InvalidExperiment(
                         "Configuration makes reference to an environment key"
                         " that does not exist: {}".format(env_key))
-                conf[key] = env.get(env_key)
+                conf[key] = env.get(env_key, env_default)
         else:
             conf[key] = value
 
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -13,8 +13,14 @@ def test_should_load_configuration():
         "token2": {
             "type": "env",
             "key": "KUBE_TOKEN"
+        },
+        "token3": {
+            "type": "env",
+            "key": "UNDEFINED",
+            "default": "value3"
         }
     })
 
     assert config["token1"] == "value1"
     assert config["token2"] == "value2"
+    assert config["token3"] == "value3"
