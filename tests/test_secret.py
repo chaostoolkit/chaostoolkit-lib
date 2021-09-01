ad585da2ae921c932d4de004b35bf31f843043cf
Kevin McKenzie <kmckenzi@us.ibm.com>
2021-09-01 15:07:59 -0400

    Resolves issue #209
    This allows for indefinitely nested secrets files.  I also address #212, so if you override a secret loaded from a vault, it will be overridden the same way a secret loaded from the environment is overridden.  This is for consistency’s sake, but if there’s a reason to preserve the old behavior, that could be done.  I also made some changes to a couple of testcases to use the load_secrets function as opposed to the load_secrets_from_vault function.
    
    Signed-off-by: Kevin McKenzie <kmckenzi@us.ibm.com>

--- tests/test_secret.py
+++ tests/test_secret.py
@@ -19,6 +19,26 @@ def test_should_load_environment():
     assert secrets["kubernetes"]["api_server_url"] == "http://1.2.3.4"
 
 
+@patch.dict(os.environ, {"KUBE_API_URL": "http://1.2.3.4"})
+def test_should_load_nested_environment():
+    secrets = load_secrets(
+        {
+            "kubernetes": {
+                "env1": {
+                    "username": "jane",
+                    "address": {"host": "whatever", "port": 8090},
+                    "api_server_url": {"type": "env", "key": "KUBE_API_URL"},
+                }
+            }
+        },
+        config.EmptyConfig,
+    )
+    assert secrets["kubernetes"]["env1"]["username"] == "jane"
+    assert secrets["kubernetes"]["env1"]["address"]["host"] == "whatever"
+    assert secrets["kubernetes"]["env1"]["address"]["port"] == 8090
+    assert secrets["kubernetes"]["env1"]["api_server_url"] == "http://1.2.3.4"
+
+
 def test_should_load_inline():
     secrets = load_secrets(
         {"kubernetes": {"api_server_url": "http://1.2.3.4"}}, config.EmptyConfig
@@ -160,7 +180,7 @@ def test_read_secrets_from_vault_with_kv_version_1(hvac):
     hvac.Client.return_value = fake_client
     fake_client.secrets.kv.v1.read_secret.return_value = vault_secret_payload
 
-    secrets = load_secrets_from_vault(secrets_info, config)
+    secrets = load_secrets(secrets_info, config)
     assert secrets["k8s"]["a-secret"] == {
         "my-important-secret": "bar",
         "my-less-important-secret": "baz",
@@ -176,7 +196,7 @@ def test_read_secrets_from_vault_with_kv_version_1(hvac):
         }
     }
 
-    secrets = load_secrets_from_vault(secrets_info, config)
+    secrets = load_secrets(secrets_info, config)
     assert secrets["k8s"]["a-secret"] == "bar"
 
 
@@ -207,7 +227,7 @@ def test_read_secrets_from_vault_with_kv_version_2(hvac):
     hvac.Client.return_value = fake_client
     fake_client.secrets.kv.v2.read_secret_version.return_value = vault_secret_payload
 
-    secrets = load_secrets_from_vault(secrets_info, config)
+    secrets = load_secrets(secrets_info, config)
     assert secrets["k8s"]["a-secret"] == {
         "my-important-secret": "bar",
         "my-less-important-secret": "baz",
@@ -223,7 +243,7 @@ def test_read_secrets_from_vault_with_kv_version_2(hvac):
         }
     }
 
-    secrets = load_secrets_from_vault(secrets_info, config)
+    secrets = load_secrets(secrets_info, config)
     assert secrets["k8s"]["a-secret"] == "bar"
 
 
@@ -244,3 +264,105 @@ def test_should_override_load_inline_with_var():
         {"kubernetes": {"api_server_url": "http://elsewhere"}},
     )
     assert secrets["kubernetes"]["api_server_url"] == "http://elsewhere"
+
+
+@patch("chaoslib.secret.hvac")
+def test_vault_add_subkeys(hvac):
+    config = {
+        "vault_addr": "http://someaddr.com",
+        "vault_token": "not_awesome_token",
+        "vault_kv_version": "2",
+    }
+
+    vault_secret_payload = {
+        "data": {
+            "data": {"foo": "bar", "baz": "hello"},
+            "metadata": {
+                "auth": None,
+                "lease_duration": 2764800,
+                "lease_id": "",
+                "renewable": False,
+            },
+        }
+    }
+
+    fake_client = MagicMock()
+    hvac.Client.return_value = fake_client
+    fake_client.secrets.kv.v2.read_secret_version.return_value = vault_secret_payload
+
+    secrets = load_secrets(
+        {"myapp": {"token": {"type": "vault", "path": "secrets/something"}}}, config
+    )
+    assert secrets["myapp"]["token"]["foo"] == "bar"
+    assert secrets["myapp"]["token"]["baz"] == "hello"
+
+
+@patch("chaoslib.secret.hvac")
+def test_vault_replace_entire_declare(hvac):
+    config = {
+        "vault_addr": "http://someaddr.com",
+        "vault_token": "not_awesome_token",
+        "vault_kv_version": "2",
+    }
+
+    vault_secret_payload = {
+        "data": {
+            "data": {"foo": "bar", "baz": "hello"},
+            "metadata": {
+                "auth": None,
+                "lease_duration": 2764800,
+                "lease_id": "",
+                "renewable": False,
+            },
+        }
+    }
+
+    fake_client = MagicMock()
+    hvac.Client.return_value = fake_client
+    fake_client.secrets.kv.v2.read_secret_version.return_value = vault_secret_payload
+
+    secrets = load_secrets(
+        {
+            "myapp": {
+                "token": {"type": "vault", "path": "secrets/something", "key": "foo"}
+            }
+        },
+        config,
+    )
+    assert secrets["myapp"]["token"] == "bar"
+
+
+@patch("chaoslib.secret.hvac")
+def test_override_vault_with_vars(hvac):
+    config = {
+        "vault_addr": "http://someaddr.com",
+        "vault_token": "not_awesome_token",
+        "vault_kv_version": "2",
+    }
+
+    vault_secret_payload = {
+        "data": {
+            "data": {"foo": "bar", "baz": "hello"},
+            "metadata": {
+                "auth": None,
+                "lease_duration": 2764800,
+                "lease_id": "",
+                "renewable": False,
+            },
+        }
+    }
+
+    fake_client = MagicMock()
+    hvac.Client.return_value = fake_client
+    fake_client.secrets.kv.v2.read_secret_version.return_value = vault_secret_payload
+
+    secrets = load_secrets(
+        {
+            "myapp": {
+                "token": {"type": "vault", "path": "secrets/something", "key": "foo"}
+            }
+        },
+        config,
+        {"myapp": {"token": "baz"}},
+    )
+    assert secrets["myapp"]["token"] == "baz"
